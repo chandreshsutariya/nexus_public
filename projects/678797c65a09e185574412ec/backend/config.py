2d4d82a1f3640c3ae224cad56eb3b31fd37e09b2
NI3singh <itsnits333@gmail.com>
2025-01-23 10:19:48 +0530

    code copied from directory downlaod and pushing again

--- projects/678797c65a09e185574412ec/backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    UPLOAD_FOLDER = 'data/temp'
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Limit upload size to 16MB
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
